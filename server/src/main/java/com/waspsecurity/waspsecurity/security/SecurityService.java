46277e4c4dd3b1631878db20fa918eebe1ae18d8
hamzafa1d1 <hamza.faidi@supcom.tn>
2024-01-10 14:57:00 +0100

    cleanups

--- server/src/main/java/com/waspsecurity/waspsecurity/security/SecurityService.java
+++ server/src/main/java/com/waspsecurity/waspsecurity/security/SecurityService.java
@@ -18,74 +18,6 @@ public class SecurityService {
     UserRepository userRepository;
     @Inject
     Argon2Utils argon2Utils;
-    @Inject
-    SecurityContext securityContext;
-
-    /*
-    public void create(Employee employee) {
-        if (employeeRepository.findById(employee.getEmail()).isPresent()) {
-            throw new EmployeeNotFoundException("Employee with email  " + employee.getEmail());
-        } else {
-            employee.hashPassword(employee.getPassword(), argon2Utils);
-            employeeRepository.save(employee);
-        }
-    }
-
-    public void delete(String email) {
-        employeeRepository.deleteById(email);
-    }
-
-    public void updatePassword(String email, Employee dto) {
-        final Principal principal = securityContext.getUserPrincipal();
-        if (isForbidden(email, securityContext, principal)) {
-            throw new EmployeeForbiddenException("Forbidden");
-        }
-        final Employee employee = employeeRepository.findById(email)
-                .orElseThrow(() -> new EmployeeNotFoundException(email));
-        employee.hashPassword(dto.getPassword(), argon2Utils);
-        employeeRepository.save(employee);
-
-    }
-
-    public Employee getUser() {
-        final Principal principal = securityContext.getUserPrincipal();
-        if (principal == null) {
-            throw new EmployeeNotAuthorizedToCreateAccountException("Employee NOT AUTHORIZED!");
-        }
-        final Employee employee = employeeRepository.findById(principal.getName())
-                .orElseThrow(() -> new EmployeeNotFoundException(principal.getName()));
-        return employee;
-    }
-
-    public void addRole(String email, Role role) {
-        final Employee employee = employeeRepository.findById(email)
-                .orElseThrow(() -> new EmployeeNotFoundException(email));
-
-        Set<Role> roles = employee.getRoles() ;
-        roles.add(role) ;
-        employee.setRoles(roles);
-        employeeRepository.save(employee);
-    }
-
-    public void removeRole(String  email , Role role) {
-        final Employee user = employeeRepository.findById(email)
-
-                .orElseThrow(() -> new EmployeeAlreadyExistsException(email));
-
-        Set<Role> roles=user.getRoles() ;
-        roles.remove(role) ;
-        user.setRoles(roles);
-        employeeRepository.save(user);
-    }
-
-    public List<Employee> getUsers() {
-        return employeeRepository.findAll().collect(Collectors.toList());
-    }
-    */
-    private boolean isForbidden(String  email, SecurityContext context, Principal principal) {
-        return !(context.isUserInRole(Role.ADMIN.name()));
-
-    }
 
     public User findBy(String email) {
         return userRepository.findByEmail(email)
@@ -93,18 +25,11 @@ public class SecurityService {
     }
 
     public User findBy(String email, String password) {
-        System.out.println("------------------------------------------------");
-        System.out.println(userRepository.findById(email).toString());
         final User user = userRepository.findById(email)
                 .orElseThrow(() -> new EmployeeNotAuthorizedException("Unauthorized"));
-        System.out.println(argon2Utils.check(user.getPassword() ,password.toCharArray()));
         if (argon2Utils.check(user.getPassword() ,password.toCharArray() )) {
-
             return user;
         }
         throw new EmployeeNotAuthorizedException("Unauthorized");
-
     }
-
-
 }
